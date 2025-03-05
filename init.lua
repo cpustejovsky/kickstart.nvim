5782a7abaa42e4480d3cfb9be8191164159a8322
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-03-05 12:20:01 -0500

    “2025-03-05_12:20:01”

--- init.lua
+++ init.lua
@@ -593,7 +593,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
+        gopls = {
+          gofumpt = true,
+          usePlaceholders = true,
+        },
         pyright = {},
         rust_analyzer = {},
 
